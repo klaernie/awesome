0334982e6c9ea1835bb5f7de462faacdd08056de
Andre Klaerner (C5147403) <andre.klaerner@sap.com>
2012-02-11 19:42:49 +0100

    fix key for kill client, fix pidgin

--- rc.lua
+++ rc.lua
@@ -123,7 +123,7 @@ shifty.config.apps = {
 		screen	= 1
 	},
 	{
-		match = {"Buddy-Liste", "pidgin", "Pidgin" },
+		match = {"pidgin", "Pidgin" },
 		tag	= "chat",
 		screen	= 1
 	},
@@ -360,7 +360,7 @@ globalkeys = awful.util.table.join(
 
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ modkey,           }, "c",      function (c) c:kill()                         end),
+    awful.key({ modkey,           }, "q",      function (c) c:kill()                         end),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
