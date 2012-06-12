22f5f8224bd358cedd74439013e6f575efc948c8
Andre Klärner <kandre@ak-online.be>
2012-06-12 07:08:07 +0200

    update time in an 1sec interval and show seconds

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -261,7 +261,7 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock({ align = "right" })
+mytextclock = awful.widget.textclock({ align = "right" }, " %a %b %d, %H:%M:%S ", 1 )
 
 -- Create a systray
 mysystray = widget({ type = "systray" })
