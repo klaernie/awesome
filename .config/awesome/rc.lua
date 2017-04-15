b4dfe4f38c895d8e20637d714cb666c295fb95be
Andre Klärner <kandre@ak-online.be>
2017-04-15 22:18:41 +0200

    borderless maximized stolen from copycat-killer/awesome-copycats

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -663,6 +663,18 @@ client.connect_signal("mouse::enter", function(c)
     end
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+-- No border for maximized clients
+client.connect_signal("focus", function(c)
+    if c.maximized then -- no borders if only 1 client visible
+        c.border_width = 0
+    elseif #awful.screen.focused().clients > 1 then
+        c.border_width = beautiful.border_width
+        c.border_color = beautiful.border_focus
+    end
+end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+
 -- }}}
+
+-- vim: set ai tabstop=4 sw=4 expandtab:
